7021be757dad554f962b003bddd82bef81431bf3
David <davidcprince01@gmail.com>
2023-05-27 05:39:42 +1200

    Centered red square

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
 import "./style.css";
 
+const width = 300;
+const height = 500;
+
 class MyGame extends Phaser.Scene {
   constructor() {
     super();
@@ -12,24 +15,41 @@ class MyGame extends Phaser.Scene {
   }
 
   create() {
-    const logo = this.add.image(400, 150, "logo");
+    this.scale.displaySize.setAspectRatio(width / height);
+    this.scale.refresh();
+    const squareWidth = 100;
+    const square = this.add.rectangle(
+      this.scale.width / 2,
+      this.scale.height / 2,
+      squareWidth,
+      squareWidth,
+      0xff0000
+    );
+    // const logo = this.add.image(
+    //   this.scale.displaySize.width,
+    //   this.scale.displaySize.height / 2,
+    //   "logo"
+    // );
 
-    this.tweens.add({
-      targets: logo,
-      y: 450,
-      duration: 2000,
-      ease: "Power2",
-      yoyo: true,
-      loop: -1,
-    });
+    // this.tweens.add({
+    //   targets: square,
+    //   y: 450,
+    //   duration: 2000,
+    //   ease: "Power2",
+    //   yoyo: true,
+    //   loop: -1,
+    // });
   }
 }
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
+  parent: "block-builder-container",
+  width,
+  height,
+  scale: {
+    mode: Phaser.Scale.FIT,
+  },
   scene: MyGame,
 };
 
